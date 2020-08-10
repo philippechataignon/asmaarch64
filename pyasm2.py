2360f067f25c77e86ac4c4a8cacab47d9f0194e5
Username <user@example.com>
2020-08-10 12:37:44 +0200

    Ajoute types

--- pyasm2.py
+++ pyasm2.py
@@ -4,13 +4,14 @@ import mmap
 
 src = open("add2.bin", "r+b")
 buf = mmap.mmap(src.fileno(), 0, prot=mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC)
-
-ftype = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int, ctypes.c_long)
+argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_long)
+ftype = ctypes.CFUNCTYPE(*argtypes)
 fpointer = ctypes.c_void_p.from_buffer(buf)
-
 f = ftype(ctypes.addressof(fpointer))
-r = f(422342, 212343, 12345678)
-print(r, 422342 * 212343 + 12345678)
+f.argtypes = argtypes
+f.restype = ctypes.c_long
+r = f(422342, 212343, 123456789090)
+print(r, 422342 * 212343 + 123456789090)
 
 del fpointer
 buf.close()
