1380842aa3057e521db9fcae695ad440c24f6341
Username <user@example.com>
2020-08-13 15:00:17 +0200

    Change test order

--- add4.s
+++ add4.s
@@ -20,13 +20,14 @@ add4:
 .loop1:
     # load array[i] = [x4 + 4w2] in x3
     # sxtw = (s)igned e(xt)end (w)ord
+    # ldrsw = load (s)igned (w)ord = 32 bits
 	ldrsw	x3, [x4, w2, sxtw #2]
     # i++
 	add	w2, w2, 1
     # x0 = accumulator
 	add	x0, x0, x3
-    # i < narray = w1 -> .L3
-	cmp	w1, w2
-	bgt	.loop1
+    # loop if w2 = i < w1 = narray
+	cmp	w2, w1
+	blt	.loop1
 .end:
 	ret
