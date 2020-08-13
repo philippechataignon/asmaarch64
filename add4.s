c1a0372639bb295905ff0b363f1816bbb8c492d4
Username <user@example.com>
2020-08-13 11:39:32 +0200

    Remove gcc instructions

--- add4.s
+++ add4.s
@@ -1,26 +1,23 @@
 	.arch armv8-a
 	.file	"add4.c"
 	.text
-	.align	2
-	.p2align 3,,7
+	.p2align 3
 	.global	add4
 	.type	add4, %function
+
 add4:
-.LFB0:
-	.cfi_startproc
     # x4 = array
 	mov	x4, x0
     # x0 = accumulator
 	mov	x0, 0
     # w1 = narray
-    # if zero, exit
+    # if zero or less, exit
 	cmp	w1, 0
-	ble	.L4
-    # init x0, w2
+	ble	.end
     # w2 = loop index i
 	mov	x2, 0
 	.p2align 3
-.L3:
+.loop1:
     # load array[i] = [x4 + 4x2] in x3
 	ldrsw	x3, [x4, x2, lsl 2]
     # i++
@@ -29,12 +26,6 @@ add4:
 	add	x0, x0, x3
     # i < narray = w1 -> .L3
 	cmp	w1, w2
-	bgt	.L3
-	.p2align 2
-.L4:
+	bgt	.loop1
+.end:
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	add4, .-add4
-	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
-	.section	.note.GNU-stack,"",@progbits
