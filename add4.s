0c9c10d47bdf6419019fcc772e3dac741e8d760d
Username <user@example.com>
2020-08-13 12:17:08 +0200

    Use w2 instead of x2

--- add4.s
+++ add4.s
@@ -15,13 +15,14 @@ add4:
 	cmp	w1, 0
 	ble	.end
     # w2 = loop index i
-	mov	x2, 0
+	mov	w2, 0
 	.p2align 3
 .loop1:
-    # load array[i] = [x4 + 4x2] in x3
-	ldrsw	x3, [x4, x2, lsl 2]
+    # load array[i] = [x4 + 4w2] in x3
+    # sxtw = (s)igned e(xt)end (w)ord
+	ldrsw	x3, [x4, w2, sxtw #2]
     # i++
-	add	x2, x2, 1
+	add	w2, w2, 1
     # x0 = accumulator
 	add	x0, x0, x3
     # i < narray = w1 -> .L3
