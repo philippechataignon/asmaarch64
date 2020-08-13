7c0a246385b667a2f41c0d6e21a2a29152e0f70b
Username <user@example.com>
2020-08-13 11:31:04 +0200

    Only one ret

--- add4.s
+++ add4.s
@@ -10,30 +10,28 @@ add4:
 	.cfi_startproc
     # x4 = array
 	mov	x4, x0
+    # x0 = accumulator
+	mov	x0, 0
     # w1 = narray
     # if zero, exit
 	cmp	w1, 0
 	ble	.L4
-    # init x0, x2
-    # x2 = loop index i
+    # init x0, w2
+    # w2 = loop index i
 	mov	x2, 0
-    # x0 = accumulator
-	mov	x0, 0
 	.p2align 3
 .L3:
     # load array[i] = [x4 + 4x2] in x3
 	ldrsw	x3, [x4, x2, lsl 2]
     # i++
 	add	x2, x2, 1
-    # i < narray = w1 -> .L3
-	cmp	w1, w2
     # x0 = accumulator
 	add	x0, x0, x3
+    # i < narray = w1 -> .L3
+	cmp	w1, w2
 	bgt	.L3
-	ret
 	.p2align 2
 .L4:
-	mov	x0, 0
 	ret
 	.cfi_endproc
 .LFE0:
