9f64d18f5a1f1b871bcc00670fc1626d68be1117
Philippe Chataignon <philippe@ldlc>
2021-07-20 17:28:47 +0200

    Rewrite add4.s

--- add4.s
+++ add4.s
@@ -1,33 +1,49 @@
-	.arch armv8-a
 	.file	"add4.c"
 	.text
-	.p2align 3
-	.global	add4
-	.type	add4, %function
-
+	.p2align 4
+	.globl	add4
+	.type	add4, @function
 add4:
-    # x4 = array
-	mov	x4, x0
-    # x0 = accumulator
-	mov	x0, 0
-    # w1 = narray
-    # if zero or less, exit
-	cmp	w1, 0
-	ble	.end
-    # w2 = loop index i
-	mov	w2, 0
+.LFB0:
+	.cfi_startproc
+	endbr64
+	testl	%esi, %esi
+	jle	.L4
+	leal	-1(%rsi), %eax
+	leaq	4(%rdi,%rax,4), %rcx
+	xorl	%eax, %eax
+	.p2align 4,,10
 	.p2align 3
-.loop1:
-    # load array[i] = [x4 + 4w2] in x3
-    # sxtw = (s)igned e(xt)end (w)ord
-    # ldrsw = load (s)igned (w)ord = 32 bits
-	ldrsw	x3, [x4, w2, sxtw #2]
-    # i++
-	add	w2, w2, 1
-    # x0 = accumulator
-	add	x0, x0, x3
-    # loop if w2 = i < w1 = narray
-	cmp	w2, w1
-	blt	.loop1
-.end:
+.L3:
+	movslq	(%rdi), %rdx
+	addq	$4, %rdi
+	addq	%rdx, %rax
+	cmpq	%rcx, %rdi
+	jne	.L3
 	ret
+	.p2align 4,,10
+	.p2align 3
+.L4:
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add4, .-add4
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
