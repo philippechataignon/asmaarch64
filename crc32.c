5a70d6a27304d325208f8f8b2f267a1c6f656d32
Username <user@example.com>
2021-07-24 12:58:34 +0200

    Optimize crc32

--- crc32.c
+++ crc32.c
@@ -11,28 +11,40 @@
 
 #define SIZE 65536
 
-int main(int argc, char* argv[]) {
-    uint8_t buff[SIZE];
-    uint8_t num = atoi(argv[1]);
-    size_t bytes;
-    uint32_t crc = 0xffffffff;
-    uint32_t (*fun)(const uint8_t*, size_t, uint32_t);
-    switch(num) {
-        case 1: fun = &crc32_ver1; crc = 0; break;
-        case 2: fun = &crc32_ver2; break;
-        case 3: fun = &crc32_ver3; break;
-        case 4: fun = &crc32_hw1; break;
-        case 5: fun = &crc32_hw2; break;
-    }
-    if (argc == 2) {
-        while(!feof(stdin) && !ferror(stdin)) {
-            bytes = fread(buff, 1, SIZE, stdin);
-            crc = (*fun)(buff, bytes, crc);
-        }
-        if (num == 1) {
-            crc = ~crc;
-        }
-        printf("Final result: %08x\n", ~crc);
-    }
-    return 0;
+int main(int argc, char *argv[])
+{
+	uint8_t buff[SIZE];
+	uint8_t num = atoi(argv[1]);
+	size_t bytes;
+	uint32_t crc = 0xffffffff;
+	uint32_t(*fun) (const uint8_t *, size_t, uint32_t);
+	switch (num) {
+	case 1:
+		fun = &crc32_ver1;
+		crc = 0;
+		break;
+	case 2:
+		fun = &crc32_ver2;
+		break;
+	case 3:
+		fun = &crc32_ver3;
+		break;
+	case 4:
+		fun = &crc32_hw1;
+		break;
+	case 5:
+		fun = &crc32_hw2;
+		break;
+	}
+	if (argc == 2) {
+		while (!feof(stdin) && !ferror(stdin)) {
+			bytes = fread(buff, 1, SIZE, stdin);
+			crc = (*fun) (buff, bytes, crc);
+		}
+		if (num == 1) {
+			crc = ~crc;
+		}
+		printf("Final result: %08x\n", ~crc);
+	}
+	return 0;
 }
