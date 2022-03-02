29f84df9f64c14aa2d73b301beb83a803eb87aa5
Username <user@example.com>
2022-03-02 22:58:54 +0100

    Add hello2

---
+++ hello2.s
@@ -0,0 +1,25 @@
+.nolist
+.include "syscall.s"
+.list
+
+.globl _start
+.text
+# Our application's entry point
+_start:
+
+    mov     x0,#1          // fd := STDOUT_FILENO 
+    ldr     x1,=msg        // buf := msg 
+    ldr     x2,=len        // count := len 
+    mov     w8,__NR_write  // syscall 
+    svc     #0             // invoke syscall 
+
+    mov     x0,#0          // status := 0 
+    mov     w8,__NR_exit   // syscal l
+    svc     #0             // invoke syscall 
+
+.data
+
+msg0:   .asciz        "Offset\n"
+msg:    .asciz        "Hello, ARM64!\n"
+len = . - msg
+
